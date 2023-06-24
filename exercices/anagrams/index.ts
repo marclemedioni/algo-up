6986966d6304aac623f5ec0180da9d60488355e0
marclemedioni <marc@lemedioni.fr>
2023-06-24 15:02:02 +0200

    (exercices) Add anagrams exercice

---
+++ exercices/anagrams/index.ts
@@ -0,0 +1,13 @@
+// Determine whether two given strings are anagrams of each other.
+// An anagram is formed when both strings contain the same characters,
+// with the same frequency, disregarding spaces, punctuation,
+// and considering capital and lowercase letters as identical.
+
+// Example -
+// anagrams('listen', 'silent') -> true
+// anagrams('HATRED', 'red hat') -> true
+// anagrams('fizz', 'buzz') -> false
+
+const anagrams = (strA: string, strB: string) => {};
+
+export default anagrams;
---
+++ tests/anagrams.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from "vitest";
+
+import anagrams from "../exercices/anagrams";
+
+test("anagrams function exists", () => {
+  expect(typeof anagrams).toEqual("function");
+});
+
+test("anagrams function returns a boolean", () => {
+  expect(typeof anagrams("hello", "llohe")).toEqual("boolean");
+});
+
+test('"hello" is an anagram of "llohe"', () => {
+  expect(anagrams("hello", "llohe")).toEqual(true);
+});
+
+test('"Whoa! Hi!" is an anagram of "Hi! Whoa!"', () => {
+  expect(anagrams("Whoa! Hi!", "Hi! Whoa!")).toEqual(true);
+});
+
+test('"One One" is not an anagram of "Two two two"', () => {
+  expect(anagrams("One One", "Two two two")).toEqual(false);
+});
+
+test('"One one" is not an anagram of "One one c"', () => {
+  expect(anagrams("One one", "One one c")).toEqual(false);
+});
+
+test('"A tree, a life, a bench" is not an anagram of "A tree, a fence, a yard"', async () => {
+  expect(anagrams("A tree, a life, a bench", "A tree, a fence, a yard")).toEqual(false);
+});
