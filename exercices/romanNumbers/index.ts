40db45d9157ea6ebc3a8f2550ff4462a5cc18b5c
marclemedioni <marc@lemedioni.fr>
2023-06-24 16:36:12 +0200

    (exercices) Add romanNumbers exercice

---
+++ exercices/romanNumbers/index.ts
@@ -0,0 +1,28 @@
+// Given a Roman Numeral as a string return its value as an integer
+// Then do the opposite with integer to roman
+
+// List of Symbols - Values
+// Symbol    Value
+//  I          1
+//  V          5
+//  X          10
+//  L          50
+//  C          100
+//  D          500
+//  M          1.000
+
+// Example
+// romanToInt("XXI") => 21
+// romanToInt("MDCLXVI") => 1666
+// romanToInt("MMVIII") => 2008
+// romanToInt("MCMXC") =>  1990
+
+// Example
+// intToRoman(21) => "XXI"
+// intToRoman(1666) => "MDCLXVI"
+// intToRoman(2008) => "MMVIII"
+// intToRoman(1990) => "MCMXC"
+
+export const romanToInt = (roman: string): number => {};
+
+export const intToRoman = (n: number): string => {};
